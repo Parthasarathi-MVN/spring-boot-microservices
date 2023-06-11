f3016971ad08659c711cfc72e53f0b61f60736f7
Parthasarathi Rao MVN <mvnparthasarathi@gmail.com>
2023-06-11 11:07:17 +0530

    Created all the required services.

---
+++ order-service/src/main/java/com/microservice/orderservice/service/OrderService.java
@@ -0,0 +1,42 @@
+package com.microservice.orderservice.service;
+
+import com.microservice.orderservice.dto.OrderLineItemsDto;
+import com.microservice.orderservice.dto.OrderRequest;
+import com.microservice.orderservice.model.Order;
+import com.microservice.orderservice.model.OrderLineItems;
+import com.microservice.orderservice.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest)
+    {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList().stream()
+                .map(orderLineItemsDto -> mapToDto(orderLineItemsDto)).toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
