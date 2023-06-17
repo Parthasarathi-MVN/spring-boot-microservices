3a893f7247f7af2ec805e5da22e3379397b40cee
Parthasarathi Rao MVN <mvnparthasarathi@gmail.com>
2023-06-17 15:22:24 +0530

    Configured Circuit Breaker, Time Limiter and Retry using Resilience4J

--- inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/microservice/inventoryservice/service/InventoryService.java
@@ -3,6 +3,7 @@ package com.microservice.inventoryservice.service;
 import com.microservice.inventoryservice.dto.InventoryResponse;
 import com.microservice.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,13 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Slf4j
 public class InventoryService {
 
     @Autowired
     private InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode) {
+    public List<InventoryResponse> isInStock(List<String> skuCode) throws InterruptedException {
+
+//        To check Timeout and Retry features of Resilience4J
+//        log.info("Wait Started");
+//        Thread.sleep(10000);
+//        log.info("Wait Ended");
 
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(inventory ->
--- order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/microservice/orderservice/controller/OrderController.java
@@ -2,6 +2,9 @@ package com.microservice.orderservice.controller;
 
 import com.microservice.orderservice.dto.OrderRequest;
 import com.microservice.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,8 +18,16 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
     public String placeOrder(@RequestBody OrderRequest orderRequest) throws IllegalAccessException {
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
+    }
+
+    public String fallBackMethod(@RequestBody OrderRequest orderRequest, RuntimeException runtimeException)
+    {
+        return "Oops! It's not you, it's us. Please order after sometime :)";
     }
 }
